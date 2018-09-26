e0823f8de88ff1abf6a8c11acdd98830a5ec29fc
unknown <200381290@student.georgianc.on.ca>
2018-09-26 15:08:43 -0400

    first commit

---
+++ Lab1-HienLam/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1_HienLam.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products of you
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Products/Product Details 
+        public ActionResult Details(string ProductName)
+        {
+            ViewBag.ProductName = ProductName;
+            return View(); 
+        }
+    }
+}
